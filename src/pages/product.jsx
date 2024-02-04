fa18b6d9576acf21e0522294fb168847c23e7b7f
dhearr <dhearr27@gmail.com>
2024-02-04 19:56:49 +0700

    Rendering List

--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -1,28 +1,44 @@
 import CardProduct from "../components/fragments/CardProduct";
 
+const products = [
+  {
+    id: 1,
+    name: "Sepatu Baru",
+    price: "Rp. 1.000.000",
+    image: "/public/images/shoes.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum facere voluptatum, voluptates nostrum ex illo, fugiat obcaecati perspiciatis porro doloribus corporis natus iure nesciunt neque perferendis velit veritatis laborum repellendus.",
+  },
+  {
+    id: 2,
+    name: "Sepatu Lama",
+    price: "Rp. 2.000.000",
+    image: "/public/images/shoes.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum facere voluptatum, voluptates nostrum ex illo, fugiat obcaecati.",
+  },
+  {
+    id: 3,
+    name: "Sepatu Bekas",
+    price: "Rp. 3.000.000",
+    image: "/public/images/shoes.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum facere voluptatum, voluptates nostrum ex illo.",
+  },
+];
+
 const ProductPage = () => {
   return (
     <div className="flex justify-center py-5">
-      <CardProduct>
-        <CardProduct.Header image="/public/images/shoes.jpg" />
-        <CardProduct.Body title="Sepatu Baru">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
-          facere voluptatum, voluptates nostrum ex illo, fugiat obcaecati
-          perspiciatis porro doloribus corporis natus iure nesciunt neque
-          perferendis velit veritatis laborum repellendus.
-        </CardProduct.Body>
-        <CardProduct.Footer price="Rp. 1.000.000" />
-      </CardProduct>
-      <CardProduct>
-        <CardProduct.Header image="/public/images/shoes.jpg" />
-        <CardProduct.Body title="Sepatu Baru">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
-          facere voluptatum, voluptates nostrum ex illo, fugiat obcaecati
-          perspiciatis porro doloribus corporis natus iure nesciunt neque
-          perferendis velit veritatis laborum repellendus.
-        </CardProduct.Body>
-        <CardProduct.Footer price="Rp. 1.000.000" />
-      </CardProduct>
+      {products.map((product) => (
+        <CardProduct key={product.id}>
+          <CardProduct.Header image={product.image} />
+          <CardProduct.Body title={product.name}>
+            {product.description}
+          </CardProduct.Body>
+          <CardProduct.Footer price={product.price} />
+        </CardProduct>
+      ))}
     </div>
   );
 };
